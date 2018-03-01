0ae1f905cf3190eab89d6b3e3df3349d71ff5409
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-03-01 17:25:44 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config.js');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+app.use(webpackDevMiddleware(webpack(webpackConfig)));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// express / post
+app.post('/', (req, res) => {
+  const { Body, From, MediaUrl0 } = req.body;
+  const message = {
+    body: Body,
+    from: From,
+    img: MediaUrl0
+  };
+  io.emit('message', message);
+  res.send(
+    `<Response>
+      <Message>Thanks for texting!</Message>
+    </Response>`
+  );
+});
+
+io.on('connection', socket => {
+  socket.on('message', body => {
+    socket.broadcast.emit('message', {
+      body,
+      from: socket.id
+    });
+  });
+});
+
+server.listen(3000);
